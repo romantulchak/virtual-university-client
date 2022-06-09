b295a9cf652d76281444117d9258ecd390f8ac8e
romantulchak <romantulchak29@gmail.com>
2022-06-09 17:46:21 +0300

    Changes: - Changed design for edit lesson dialog - Code optimization

--- src/app/edit-lesson/edit-lesson.component.ts
+++ src/app/edit-lesson/edit-lesson.component.ts
@@ -1,39 +1,99 @@
-import { AfterContentInit, Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { SubjectTeacherGroupDTO } from '../dto/subjectTeacherGroup.dto';
-import { StudentGroupService } from '../service/student-group.service';
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {SubjectTeacherGroupDTO} from '../dto/subjectTeacherGroup.dto';
+import {StudentGroupService} from '../service/student-group.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-lesson',
   templateUrl: './edit-lesson.component.html',
-  styleUrls: ['./edit-lesson.component.scss']
+  styleUrls: ['./edit-lesson.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class EditLessonComponent implements OnInit,AfterContentInit {
+export class EditLessonComponent implements OnInit  {
 
   public subjectTeacher: SubjectTeacherGroupDTO[];
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private groupService: StudentGroupService) {
-    this.data.lesson.dateStart = this.data.lesson.dateStart.slice(11, 16);  
+  public editFormGroup: FormGroup;
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              private groupService: StudentGroupService,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
-  }
-  ngAfterContentInit(){
+    this.initEditFormGroup();
     this.findSubjectsForGroup();
   }
 
-  public setTimeStart(){}
-  public setTimeEnd(){}
-  public setRoomNumber(){}
-  public setSubjectToLesson(){}
-  public addLessonToDay(){}
+  public edit(): void {
 
+  }
 
-  private findSubjectsForGroup(){
+  /**
+   * Compare times depends on flag
+   *
+   * @param selectedTime time to be compared
+   * @param isDateStart if true checks if Date End is bigger than date start
+   *                    otherwise checks if Date Start is bigger than date end
+   */
+  public changeTime(selectedTime: string, isDateStart: boolean): void {
+    if (isDateStart){
+      if (this.dateEnd.value <= selectedTime){
+        this.dateEnd.setErrors({incorrect: true});
+      } else {
+        this.dateEnd.setErrors(null);
+      }
+    } else {
+      if (this.dateStart.value >= selectedTime){
+        this.dateStart.setErrors({incorrect: true});
+      } else {
+        this.dateStart.setErrors(null);
+      }
+    }
+  }
+
+  /**
+   * Inits edit form group
+   */
+  private initEditFormGroup(): void {
+    console.log(this.data);
+    this.editFormGroup = this.fb.group({
+      id: [this.data.lesson.id, Validators.required],
+      dateStart: [this.getTimeFromDate(this.data.lesson.dateStart), Validators.required],
+      dateEnd: [this.getTimeFromDate(this.data.lesson.dateEnd), Validators.required],
+      roomNumber: [this.data.lesson.roomNumber, Validators.required],
+      subject: [null, Validators.required]
+    });
+  }
+
+  /**
+   * Finds Subjects for group with Teachers
+   */
+  private findSubjectsForGroup(): void {
     this.groupService.findSubjectsForGroup(this.data.group.id, this.data.selectedSemester.id).subscribe(
-      res=>{
+      res => {
+        const subjectTeacher = res.find(subjectTeacherDTO => subjectTeacherDTO.id === this.data.lesson.subjectTeacher.id);
+        this.editFormGroup.get('subject').setValue(subjectTeacher);
         this.subjectTeacher = res;
       }
     );
+  }
+
+  /**
+   * Gets time from date. Ex: 2022-06-08T08:25 -> 08:25
+   */
+  private getTimeFromDate(value: string): string {
+    if (value) {
+      return value.slice(11, 16);
+    }
+    return '';
+  }
+
+  get dateStart(): FormControl {
+    return this.editFormGroup.get('dateStart') as FormControl;
+  }
+
+  get dateEnd(): FormControl {
+    return this.editFormGroup.get('dateEnd') as FormControl;
   }
 }
--- src/app/service/teacher.service.ts
+++ src/app/service/teacher.service.ts
@@ -1,38 +1,41 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { environment } from "src/environments/environment";
-import { SpecializationDTO } from "../dto/specialization.dto";
-import { SubjectDTO } from "../dto/subject.dto";
-import { TeacherDTO } from "../dto/teacher.dto";
-import { Teacher } from "../model/teacher.model";
-import { ResetPasswordRequest } from "../request/resetPasswordRequest.request";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { SubjectDTO } from '../dto/subject.dto';
+import { TeacherDTO } from '../dto/teacher.dto';
+import { Teacher } from '../model/teacher.model';
+import { ResetPasswordRequest } from '../request/resetPasswordRequest.request';
 
-const API_URL = environment.api;
-
+const API_URL = `${environment.apiUrl}/teacher`;
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
 export class TeacherService{
     constructor(private http: HttpClient){}
 
-    public getAllTeachers():Observable<TeacherDTO[]>{
-        return this.http.get<TeacherDTO[]>(API_URL + 'teacher');
+    public getAllTeachers(): Observable<TeacherDTO[]>{
+        return this.http.get<TeacherDTO[]>(`${API_URL}`);
     }
-    public getTeacherInformation(id: number):Observable<TeacherDTO>{
-        return this.http.get<TeacherDTO>(API_URL + 'teacher/findTeacherById/' + id);
+
+    public getTeacherInformation(id: number): Observable<TeacherDTO>{
+        return this.http.get<TeacherDTO>(`${API_URL}/find-teacher/${id}`);
     }
-    public createTeacher(teacher: Teacher):Observable<any>{
-        return this.http.post(API_URL + 'teacher/createTeacher', teacher);
+
+    public createTeacher(teacher: Teacher): Observable<any>{
+        return this.http.post(`${API_URL}/create-teacher`, teacher);
     }
-    public addSubjectsToTeacher(teacherId: number, subjects: SubjectDTO[]):Observable<any>{
-        return this.http.put(API_URL + 'teacher/addSubjectsToTeacher/' + teacherId, subjects);
+
+    public addSubjectsToTeacher(teacherId: number, subjects: SubjectDTO[]): Observable<void>{
+        return this.http.put<void>(`${API_URL}/add-subjects-to-teacher/${teacherId}`, subjects);
     }
+
     public findTeachersBySubject(subjectId: number): Observable<TeacherDTO[]>{
-        return this.http.get<TeacherDTO[]>(API_URL + 'teacher/findTeachersForSubject/' + subjectId);
+        return this.http.get<TeacherDTO[]>(`${API_URL}/find-teachers-for-subject/${subjectId}`);
     }
-    public resetPassword(resetPasswordRequest: ResetPasswordRequest):Observable<any>{
-        return this.http.put(API_URL + 'teacher/resetPassword', requestAnimationFrame);
+
+    public resetPassword(resetPasswordRequest: ResetPasswordRequest): Observable<void>{
+        return this.http.put<void>(`${API_URL}/reset-password`, resetPasswordRequest);
     }
 }
